c1ee7ac80f5da77ce43e5febb5a61158b4628770
david.marques <david.marques@closer.pt>
2020-03-21 10:24:23 +0000

    Finished the crawler

--- getcoronanews/spiders/covidSpider.py
+++ getcoronanews/spiders/covidSpider.py
@@ -1,24 +1,22 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 import re
-from datetime import datetime as dt
 from urllib.parse import urlparse
 import pandas as pd
 from unidecode import unidecode
 
-websites_data = pd.read_csv("websites.csv", sep=";", index_col=0)
+websites_data = pd.read_csv("websites.csv", index_col=0)
 
 
 class CovidSpider(CrawlSpider):
     name = "covidnews"
-    start_urls = websites_data.index.values
+    start_urls = websites_data.index.values.tolist()
     parsed_urls = [urlparse(url) for url in start_urls]
     allowed_domains = list(set(f"{url.netloc}" for url in parsed_urls))
 
     deny_links_rule = "|".join(["deporto", "economia", "cultura", "política"])
     rules = [Rule(LinkExtractor(allow=r"(corona|covid)", deny=deny_links_rule), follow=True, callback="parse_article")]
 
-    @staticmethod
     def parse_article(self, response):
         """ Parses the article and returns the proper output"""
 
@@ -26,17 +24,23 @@ class CovidSpider(CrawlSpider):
         origin_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
 
         title_path = websites_data.loc[origin_url, "path_to_title"]
-        date_path = websites_data.loc[origin_url, "path_to_date"]
         content_path = websites_data.loc[origin_url, "path_to_content"]
 
-        title = response.css(title_path).get()
+        title = response.css(title_path + "::text").get()
         content = unidecode(".".join([x.get() for x in response.css(content_path + " p ::text")]))
         content = re.sub(r"\r|\n", " ", content)
-        try:
-            datetime = response.css(date_path).attrib['datetime']
-            datetime = dt.strptime(datetime, "%Y-%m-%d %H:%M").strftime("%Y-%m-%d")
-        except:
-            datetime = None
+
+        # Try and find the date
+        datetime = re.findall(r"(\d{4}-\d{2}-\d{2}|\d{4}/\d{2}/\d{2})", response.url)
+        if len(datetime) > 0:
+            datetime = datetime[0]
+        else:
+            datetime = re.findall(r"(\d{4}-\d{2}-\d{2}|\d{4}/\d{2}/\d{2})", response.text)
+            if len(datetime) == 0:
+                datetime = None
+            else:
+                datetime = datetime[0]
+
         if title is not None:
             return {
                 "news_date": datetime,
